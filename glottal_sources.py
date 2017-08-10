e4c06ad5d7c84a0be7224ccd82b9459ac7ac17c9
jareddrayton <jareddrayton@users.noreply.github.com>
2017-08-10 21:52:29 +0100

    Planning for this module.
    Signed-off-by: jareddrayton <jared.drayton@users.noreply.github.com>

--- glottal_sources.py
+++ glottal_sources.py
@@ -1,6 +1,35 @@
-# Start with implementing Rosenberg 1971
+import numpy as np
+
+# Start with implementing Rosenbergs models from 1971
+# "Effect of Glottal Pulse Shape on the Quality of Natural Vowels"
+# What is useful output?
+# Could use as a command line application.
+# Put in desired length of audio, waveform type, F0
+# How is this used as a module? e.g. with a vocal tract model.
 
 
+
+Tp = 0
+
+def rosenberg_a():
+
+    0 < t <= Tp
+    a = t/Tp
+
+def rosenberg_b():
+    pass
+
+def rosenberg_c():
+    pass
+
+def rosenberg_d():
+    pass
+
+def rosenberg_e():
+    pass
+
+def rosenberg_f():
+    pass
 
 
 # LF model
