656c507ea0f07fce07c1d0a41986f01c25770e70
jareddrayton <jareddrayton@users.noreply.github.com>
2017-10-09 14:52:42 +0100

    Fixed bug in logfbank
    Signed-off-by: jareddrayton <jareddrayton@users.noreply.github.com>

--- mfcc_comparison.py
+++ mfcc_comparison.py
@@ -8,7 +8,7 @@ import pydistance
 import os
 
 # Specify the target file
-(rate, signal) = wav.read("Target[a].wav")
+(rate, signal) = wav.read("Sounds/vowel-09.wav")
 
 # Prints the sample rate of target sound.
 print(rate)
@@ -30,46 +30,51 @@ logfbank_features_target = logfbank(signal, rate, winlen=0.025, winstep=0.025)
 print(np.shape(logfbank_features_target ))
 logfbank_averages_target = np.average(logfbank_features_target, axis=0)
 
+
+for vowel in vowels:
+    (rate, signal) = wav.read("Sounds/{!s}".format(vowel))
+    print(rate)
+
+
 def compare_mfcc(vowels):
 
     for vowel in vowels:
         (rate, signal) = wav.read("Sounds/{!s}".format(vowel))
 
         mfcc_features = mfcc(signal, rate, winlen=0.025, winstep=0.025)
-
         mfcc_averages = np.average(mfcc_features, axis=0)
 
-        distance = pydistance.SSD(mfcc_averages_target, mfcc_averages)
+        distance = pydistance.MSE(mfcc_averages_target, mfcc_averages)
+
 
         print(vowel, distance)
 
 
-compare_mfcc(vowels)
-
 def compare_fbank(vowels):
+
     for vowel in vowels:
         (rate, signal) = wav.read("Sounds/{!s}".format(vowel))
 
         fbank_features, fbank_energy = fbank(signal, rate, winlen=0.025, winstep=0.025)
         fbank_averages = np.average(fbank_features, axis=0, )
 
-        distance = pydistance.SSD(fbank_averages_target, fbank_averages)
+        distance = pydistance.MSE(fbank_averages_target, fbank_averages)
 
         print(vowel, distance)
 
 
-compare_fbank(vowels)
-
 def compare_logfbank(vowels):
+
     for vowel in vowels:
         (rate, signal) = wav.read("Sounds/{!s}".format(vowel))
 
         logfbank_features = logfbank(signal, rate, winlen=0.025, winstep=0.025)
         logfbank_averages = np.average(logfbank_features, axis=0, )
 
-        distance = pydistance.SSD(fbank_averages_target, logfbank_averages)
+        distance = pydistance.MSE(logfbank_averages_target, logfbank_averages)
 
         print(vowel, distance)
 
-
+compare_mfcc(vowels)
+compare_fbank(vowels)
 compare_logfbank(vowels)
