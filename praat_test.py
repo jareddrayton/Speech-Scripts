cf565c3ed2be394705439d48117e9d1e6e7a841a
jareddrayton <jareddrayton@users.noreply.github.com>
2017-10-10 15:17:07 +0100

    Sample rate of synthesised sounds will match that of the target sound.
    Signed-off-by: jareddrayton <jareddrayton@users.noreply.github.com>

--- praat_test.py
+++ praat_test.py
@@ -78,10 +78,59 @@ def artword_generator(index):
     f.write('''nowarn do ("Save as WAV file...", "Individual{!s}.wav")\r\n'''.format(index))
 
 
+def artword_generator_limited(index):
+    """This function generates a random artword/praat script"""
+
+    # a list containing all of the articulator parameters barring lungs
+    parameters = ['Hyoglossus',
+                  'Styloglossus',
+                  'Genioglossus',
+                  'UpperTongue',
+                  'LowerTongue',
+                  'TransverseTongue',
+                  'VerticalTongue',
+                  'Risorius',
+                  'OrbicularisOris',
+                  'TensorPalatini',
+                  'Masseter',
+                  'Mylohyoid',
+                  'LateralPterygoid',
+                  'Buccinator']
+
+    # Generate a list of n random values in the range [-1,1] with a list comprehension
+    values = [round(random.uniform(0, 1), 1) for x in range(len(parameters))]
+
+    # creates a text file with the .praat extension by calling open and assigns to variable f
+    f = open('./{}/test{}.praat'.format(temp_fp, index), 'w')
+
+    f.write('Create Speaker... Robovox Male 2\r\n')
+    f.write('Create Artword... Individual{!s} {}\r\n'.format(index, duration))
+    f.write('Set target... 0.0  0.2  Lungs\r\n')
+    f.write('Set target... 0.1  0.0  Lungs\r\n')
+    f.write('Set target... {}   0.0  Lungs\r\n'.format(duration))
+    f.write('Set target... 0.00 1 LevatorPalatini\r\n')
+    f.write('Set target... {} 1 LevatorPalatini\r\n'.format(duration))
+    f.write('Set target... 0.00 0.5 Interarytenoid\r\n')
+    f.write('Set target... {} 0.5 Interarytenoid\r\n'.format(duration))
+
+    # a loop that
+    for i in range(len(parameters)):
+        f.seek(0, 2)
+        f.write('Set target... 0.0 {!s} {!s}\r\n'.format(values[i], parameters[i]))
+        f.write('Set target... {} {!s} {!s}\r\n'.format(duration, values[i], parameters[i]))
+
+    f.write('select Artword Individual{!s}\r\n'.format(index))
+    f.write('plus Speaker Robovox\r\n')
+    f.write('To Sound... 22500 25    0 0 0    0 0 0   0 0 0\r\n')
+    f.write('''nowarn do ("Save as WAV file...", "Individual{!s}.wav")\r\n'''.format(index))
+
+
 os.mkdir(temp_fp)
 
+
 for i in range(n):
-    artword_generator(i)
+    artword_generator_limited(i)
+
 
 def praat_serial():
 
@@ -95,7 +144,7 @@ def praat_serial():
     print('Total time to synthesise {!s} sounds of duration {} in seconds = {!s}'.format(n, duration, end))
     print('This is equivalent to ', round(n * float(duration) / end, 2), 'x real time')
 
-praat_serial()
+#praat_serial()
 
 shutil.rmtree(temp_fp)
 
