d4f342f24cb90852eabfbeee6ceabd8dce2ca831
Jared <jareddrayton@users.noreply.github.com>
2018-01-25 15:46:22 +0000

    Added constants

--- glottal_sources.py
+++ glottal_sources.py
@@ -7,21 +7,29 @@ import numpy as np
 # Put in desired length of audio, waveform type, F0
 # How is this used as a module? e.g. with a vocal tract model.
 
-
-
 # Pulse train
 
 def pulse_train():
     pass
 
+
 # Rosenberg
 
-Tp = 0
+amplitude = 32767
+sample_rate = 16000
+fundamental_frequency = 120
+
+period_samples = sample_rate // fundamental_frequency
+
+Tp = 0.4 * period_samples # length of slope in 
+Tn = 0.16
+To = 1 - Tp - Tn
 
 def rosenberg_a():
-
     0 < t <= Tp
     a = t/Tp
+    a * t / Tp
+
 
 def rosenberg_b():
     pass
@@ -41,7 +49,8 @@ def rosenberg_f():
 
 # LF model
 
-
+def lf():
+    pass
 
 
 # Flanagan
