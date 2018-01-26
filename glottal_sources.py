08792aca242fd42ea540a7b057c8b07e276e5d04
unknown <jareddrayton@users.noreply.github.com>
2018-01-26 17:57:09 +0000

    Added logic for rosenberg_c(). Some faulty logic in d

--- glottal_sources.py
+++ glottal_sources.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
@@ -13,14 +14,17 @@ import matplotlib.pyplot as plt
 
 wav_length = 10.0 # Specify the length of the .Wav file in seconds
 
-amplitude = 32767 # Using 16 Bit PCM. This is just 'a' in the original paper
+amplitude = 32767 # Using 16 Bit PCM. This is 'a' in the original paper
 sample_rate = 44100 # 
 fundamental_frequency = 119 #
 
-period_samples = sample_rate // fundamental_frequency
+period_samples = sample_rate // fundamental_frequency # No. of samples used for one period
 
-Tp = int(0.4 * period_samples) # length of slope in 
-Tn = int(0.16 * period_samples)
+positive_slope = 0.40
+negative_slope = 0.16
+
+Tp = int(positive_slope * period_samples) # duration of slope in samples 
+Tn = int(negative_slope * period_samples) # duration of slope in samples
 To = int(period_samples - Tp - Tn)
 
 print(Tp, Tn, To)
@@ -28,7 +32,7 @@ print(Tp + Tn + To)
 print(period_samples)
 
 ###############################################################################
-# Rosenberg
+# Rosenberg pulse functions
 
 def rosenberg_a():
     
@@ -49,38 +53,67 @@ def rosenberg_a():
         #print(i*0)
         data.append(i*0)
 
-    test = list(map(int, data))
+    data = list(map(int, data))
     
     #print(len(test))
     #print(test)
     
-    return test
+    return data
 
 def rosenberg_b():
     
     data = []
     
-    for i in range(0, Tp): # 0 < t <= Tp
+    for i in range(0, Tp): # While 0 < t <= Tp
         data.append(amplitude * ( 3 * (i/Tp) ** 2 - 2 * (i/Tp) ** 3))
 
-    for i in range(Tp, Tp + Tn): #Tp <= t <= Tp + Tn
+    for i in range(Tp, Tp + Tn): # While Tp <= t <= Tp + Tn
         data.append(amplitude * (1 - ((i-Tp)/Tn)**2))
 
     for i in range(Tp + Tn, period_samples): # 
         data.append(i*0)
 
-    test = list(map(int, data))
+    data = list(map(int, data)) # Convert to integers
     
-    #print(len(test))
-    #print(test)
-    
-    return test
+    return data
 
 def rosenberg_c():
-    pass
+    
+    data = []
+    
+    for i in range(0, Tp): # While 0 < t <= Tp
+        data.append(    (amplitude/2.0) * (1 - math.cos((i/Tp) * math.pi)       ))
+
+    for i in range(Tp, Tp + Tn): # While Tp <= t <= Tp + Tn
+        data.append( amplitude * math.cos( ((i-Tp)/Tn) * (math.pi/2) ) )
+        #print(amplitude, i, Tp, Tn,)
+    
+    for i in range(Tp + Tn, period_samples): # 
+        data.append(i*0)
+
+    data = list(map(int, data)) # Convert to integers
+    
+    return data
 
 def rosenberg_d():
-    pass
+    
+    data = []
+    
+    for i in range(0, Tp): # While 0 < t <= Tp
+        data.append(    (amplitude/2.0) * (1 - math.cos((i/Tp) * math.pi)       ))
+
+    for i in range(Tp, Tp + Tn): # While Tp <= t <= Tp + Tn
+        data.append( amplitude/2.0 * ( 1 + math.cos( ((i-Tp)/Tn) * (math.pi/2) ) ) )
+        #print(amplitude, i, Tp, Tn,)
+        #print( amplitude/2.0 * ( 1 + math.cos( ((i-Tp)/Tn) * (math.pi/2) ) ) )
+    for i in range(Tp + Tn, period_samples): # 
+        data.append(i*0)
+
+    data = list(map(int, data)) # Convert to integers
+    
+    return data
+
+rosenberg_d()
 
 def rosenberg_e():
     pass
@@ -94,6 +127,7 @@ def rosenberg_f():
 def pulse_train():
     pass
 
+###############################################################################
 # LF model
 
 def lf():
@@ -127,7 +161,7 @@ def make_wav():
     print('WARNING: File will be short by ', 
         int(sample_rate * wav_length) - a.size, 'samples')
 
-    wav.write('test_1.wav', sample_rate, a) # Writes the numpy array to a .wav file
+    wav.write('test_a.wav', sample_rate, a) # Writes the numpy array to a .wav file
 
 #make_wav()
 
@@ -136,8 +170,10 @@ def make_wav():
 
 def make_plot():
     fig, ax = plt.subplots()
-    ax.plot(rosenberg_a())
-    ax.plot(rosenberg_b())
+    #ax.plot(rosenberg_a())
+    #ax.plot(rosenberg_b())
+    #ax.plot(rosenberg_c())
+    ax.plot(rosenberg_d())
 
     ax.set(xlabel='time (s)', ylabel='Amplitude',
        title='About as simple as it gets, folks')
@@ -146,7 +182,7 @@ def make_plot():
     #fig.savefig("test.png")
     plt.show()
 
-make_plot()
+#make_plot()
 
 def plot_all():
     pass
